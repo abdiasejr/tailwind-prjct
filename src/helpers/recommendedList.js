540279be196b2bf1aea5cc5a8e4c4bf0a6a9131d
Abdias <abdiasejr@gmail.com>
2023-02-01 20:56:36 -0300

    ✔ typo solved

---
+++ src/helpers/recommendedList.js
@@ -0,0 +1,27 @@
+export const recommendedList = [
+  {
+    bg: "bg-norway",
+    title: "Norway",
+    subtitle: "Beautiful landscapes",
+  },
+  {
+    bg: "bg-new_york",
+    title: "New York",
+    subtitle: "Concret jungle",
+  },
+  {
+    bg: "bg-yosemite",
+    title: "Yosemite",
+    subtitle: "A break from the city",
+  },
+  {
+    bg: "bg-seattle",
+    title: "Seattle",
+    subtitle: "Big city",
+  },
+  {
+    bg: "bg-switzerland",
+    title: "Switzerland",
+    subtitle: "Peaceful and quiet",
+  },
+];
