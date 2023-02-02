e86d4d41df4ba607c9e8a736e7ad7c87305493a7
Abdias <abdiasejr@gmail.com>
2023-02-01 21:02:53 -0300

    ✔ index file for exports

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import { Card } from "./Card";
+import { MainCard } from "./MainCard";
+import { Recommended } from "./Recommended";
+import { Popular } from "./Popular";
+import { FAQ } from "./FAQ";
+
+export { Card, MainCard, Recommended, Popular, FAQ };
