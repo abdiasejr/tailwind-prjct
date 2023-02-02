f1b9a1cc8e306904d4f0c5e0f98a7bb229a4dc90
Abdias <abdiasejr@gmail.com>
2023-02-01 21:01:41 -0300

    🚧 recommend section into a component

---
+++ src/components/Recommended.js
@@ -0,0 +1,17 @@
+import { recommendedList } from "../helpers/recommendedList";
+import { Card } from "./Card";
+
+export const Recommended = () => {
+  return (
+    <div id="recommended" className="flex w-full flex-col gap-5 pl-5">
+      <h2 className=" mt-7 text-2xl font-bold not-italic  text-primary">
+        Our Recomendations
+      </h2>
+      <div className=" flex h-80 w-auto flex-row items-center gap-5 overflow-x-auto overscroll-contain pr-5 ">
+        {recommendedList.map((item, i) => (
+          <Card key={i} item={item} i={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
