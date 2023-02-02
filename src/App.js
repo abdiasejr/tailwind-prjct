4016e1c30089a8eb06075a69efb232d91b293a41
Abdias <abdiasejr@gmail.com>
2023-02-01 21:03:52 -0300

    🚧 new structure for main content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import { Header } from "./components/Header";
-import { Main } from "./containers/Main";
+import { Header, Main, Footer } from "./containers";
+import { Recommended, Popular, FAQ } from "./components";
+import { faqsList } from "./helpers/faqsList";
 
 function App() {
   return (
     <div className="w-full font-Montserrat">
       <Header />
-      <Main />
+      <Main>
+        <Recommended />
+        <Popular />
+        <FAQ>
+          <div className="flex w-full flex-col gap-3 px-7">
+            {faqsList.map((item, i) => (
+              <div key={i} className="flex flex-col gap-2">
+                <h3 className="text-base font-semibold text-primary">
+                  {item.question}
+                </h3>
+                <p className="mr-8 text-xs font-medium text-black">
+                  {item.answer}
+                </p>
+              </div>
+            ))}
+          </div>
+        </FAQ>
+      </Main>
+      <Footer />
     </div>
   );
 }
