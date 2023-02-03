beec5c1eef874121a4199a21cd3233a7f2a68828
Abdias <abdiasejr@gmail.com>
2023-02-02 23:03:27 -0300

    ✔ new export

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import { MainCard } from "./MainCard";
 import { Recommended } from "./Recommended";
 import { Popular } from "./Popular";
 import { FAQ } from "./FAQ";
+import { NavBar } from "./NavBar";
 
-export { Card, MainCard, Recommended, Popular, FAQ };
+export { Card, MainCard, Recommended, Popular, FAQ, NavBar };
