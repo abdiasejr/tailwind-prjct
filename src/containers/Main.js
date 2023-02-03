16cf4f775867983bc5eca2e33fa950795c0ed6fa
Abdias <abdiasejr@gmail.com>
2023-02-02 23:05:20 -0300

    🚧 deleting class useless to div wrapper

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,7 +4,7 @@ export const Main = ({ children }) => {
   return (
     <main className="flex w-full flex-col items-center gap-5">
       {Children.map(children, (child) => (
-        <div className="flex w-full flex-col gap-5">{child}</div>
+        <div className="flex w-full flex-col">{child}</div>
       ))}
     </main>
   );
