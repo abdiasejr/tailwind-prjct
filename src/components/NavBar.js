b423cfd7b1e36482cfa7cd60b759915ed2ff4538
Abdias <abdiasejr@gmail.com>
2023-02-02 23:00:10 -0300

    🚧 nav bar component done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,20 @@
+import { AiFillHeart, AiFillHome } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { BsFillPersonFill } from "react-icons/bs";
+
 export const NavBar = () => {
-  return {
-    /* <nav className="flex w-full flex-row fixed gap-5 px-7 py-5">
-    
-    </nav> */
-  };
+  return (
+    <nav className="fixed bottom-0 left-0 z-10 flex w-full flex-row items-center justify-center gap-10 bg-white p-4 text-3xl shadow-md">
+      <a href="#home">
+        <AiFillHome className="text-gray-300 hover:text-primary" />
+      </a>
+      <a href="#popular">
+        <BiSearch className="text-primary" />
+      </a>
+      <a href="#recommended">
+        <AiFillHeart className="text-gray-300 hover:text-primary" />
+      </a>
+      <BsFillPersonFill className="text-gray-300 hover:text-primary" />
+    </nav>
+  );
 };
