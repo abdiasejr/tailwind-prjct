4e9845ee63bbcccbf155bc5440fc2b3bfd5f606b
Abdias <abdiasejr@gmail.com>
2023-02-04 00:14:43 -0300

    ✔ new nav import

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,6 @@
 import { Header } from "./Header";
 import { Main } from "./Main";
 import { Footer } from "./Footer";
+import { Nav } from "./Nav";
 
-export { Header, Main, Footer };
+export { Header, Main, Footer, Nav };
