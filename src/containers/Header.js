766576ea630cf56a0adbddde5903c2118deed3fd
Abdias <abdiasejr@gmail.com>
2023-02-03 23:55:33 -0300

    🚧 media queries for desktop

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,6 +1,6 @@
 import { IoMdMoon } from "react-icons/io";
-import { BiSearch } from "react-icons/bi";
 import { useState } from "react";
+import { SearchBar } from "../components/SearchBar";
 
 export const Header = () => {
   const [changed, setChanged] = useState(false);
@@ -11,24 +11,18 @@ export const Header = () => {
   return (
     <header
       id="home"
-      className="flex h-[30.5rem] w-full flex-col gap-5 bg-sanFrancisco bg-cover bg-center bg-no-repeat p-5"
+      className="flex h-[30.5rem] w-full flex-col gap-5 bg-sanFrancisco bg-cover bg-center bg-no-repeat p-5 md:justify-center md:bg-sanFranciscoDesktop lg:h-[42rem] "
     >
-      <button className="h-8 w-14 self-end rounded-full bg-white py-2  px-5 text-primary">
+      <button className="h-8 w-14 self-end rounded-full bg-white py-2  px-5 text-primary md:hidden">
         <IoMdMoon />
       </button>
-      <label className="relative self-center">
-        {!changed && (
-          <BiSearch className=" absolute left-14 bottom-[0.90rem] block text-xl  text-primary " />
-        )}
-        <input
-          type="search"
-          className="w-min-[20rem] peer h-12 w-72 self-center rounded-full border-none bg-white py-2 px-5 text-center shadow-lg "
-          placeholder="        San Francisco"
-          onChange={(e) => handleChange(e)}
-        />
-      </label>
+      <SearchBar changed={changed} handleChange={handleChange} />
+      <h1 className="hidden cursor-pointer justify-start text-4xl font-bold text-black transition-all hover:scale-110 md:ml-20 md:block md:w-1/2">
+        Find your perfect trip, designed by insiders who know and love their
+        cities.
+      </h1>
       <button
-        className=" mt-64 h-12 w-48 transform self-center rounded-full bg-white px-8 pt-3 pb-4 font-bold text-primary shadow-lg transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-primary hover:text-white hover:shadow-2xl
+        className=" lg: mt-64 h-12 w-48 transform self-center rounded-full bg-white px-8 pt-3 pb-4 font-bold text-primary shadow-lg transition-all  duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-primary hover:text-white hover:shadow-2xl md:mt-0 md:ml-20 md:self-start
         "
       >
         Explore More
